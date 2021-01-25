f51a859b3d354349605ce88b4fe28992db3c2715
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-01-25 15:28:19 -0300

    Arrays Types

--- 2/app.js
+++ 2/app.js
@@ -4,6 +4,14 @@
 // } = {
 var person = {
     name: 'Ludi',
-    age: 18
+    age: 18,
+    hobbies: ['Sports', 'Cooking']
 };
+var favoriteActivities;
+favoriteActivities = ['Sports'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); //ERROR !!!
+}
--- 2/app.ts
+++ 2/app.ts
@@ -4,7 +4,16 @@
 // } = {
 const person = {
     name: 'Ludi',
-    age:18
+    age:18,
+    hobbies: ['Sports', 'Cooking']
 };
 
+let favoriteActivities : string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for( const hobby of person.hobbies){
+    console.log(hobby.toUpperCase());
+   // console.log(hobby.map()); //ERROR !!!
+}
