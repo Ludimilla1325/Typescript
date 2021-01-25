4537977c830e7c9a3e9afed94ea3c3f736352d8b
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-01-25 16:02:26 -0300

    Working with tuples

--- 2/app.js
+++ 2/app.js
@@ -5,8 +5,12 @@
 var person = {
     name: 'Ludi',
     age: 18,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author'] //tuples- is like an array but with always only 2 elements
 };
+//person.role.push('admin'); //Push is an exeception which is allowed, typescript cant catch this error
+//person.role[1] = 10; The 2nd argument should be a string
+person.role = [0, 'admin']; //To work eith tuple will make ur code more strict
 var favoriteActivities;
 favoriteActivities = ['Sports'];
 console.log(person.name);
--- 2/app.ts
+++ 2/app.ts
@@ -2,11 +2,23 @@
 //     name:string;
 //     age: number;
 // } = {
-const person = {
+
+const person:{
+    name:string;
+    age: number;
+    hobbies: string[];
+    role: [number,string]; //This marks a tuple
+} = {
     name: 'Ludi',
     age:18,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author'] //tuples- is like an array but with always only 2 elements
 };
+
+//person.role.push('admin'); //Push is an exeception which is allowed, typescript cant catch this error
+//person.role[1] = 10; The 2nd argument should be a string
+
+person.role = [0, 'admin']; //To work eith tuple will make ur code more strict
 
 let favoriteActivities : string[];
 favoriteActivities = ['Sports'];
