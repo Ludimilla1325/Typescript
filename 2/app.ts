943bab1ec708cb59d35c40c7cf253aaf665d3056
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-01-26 16:35:08 -0300

    Literal Types

--- 2/app.ts
+++ 2/app.ts
@@ -1,16 +1,30 @@
 //Union Types - Use two types
-function combine(input1: number | string, input2: number | string){
-let result;
-if (typeof input1 === 'number' && typeof input2 === 'number'){
-    result = input1 + input2;
-} else {
-    result = input1.toString() + input2.toString(); 
-}  
-   return result;
-}
+function combine(
+    input1: number | string, 
+    input2: number | string, 
+    resultConvertion:  'as-number' | 'as-text'
+){
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConvertion === 'as-number'){
+        result = +input1 + +input2;
+    } else {
+        result = input1.toString() + input2.toString(); 
+    }  
 
-const combinedAges = combine(30,26);
+    return result;
+
+    // if (resultConvertion === 'as-number'){
+    //     return +result; //+ return a number, aqui está forçando para retornar um número
+    // } else
+    // return result.toString();
+    // }
+
+}
+const combinedAges = combine(30,26, 'as-number');
 console.log(combinedAges);
 
-// let combinedAges = combine('Max', 'Anna');
-// console.log(combinedAges);
+const combinedStringAges = combine('30','26', 'as-number');
+console.log(combinedStringAges);
+
+let combinedNames = combine('Max', 'Anna', 'as-text');
+console.log(combinedNames);
