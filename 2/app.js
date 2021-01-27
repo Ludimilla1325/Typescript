0decc6f88e3e4179124a1130a569691676339ada
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-01-27 10:48:48 -0300

    Function as Types

--- 2/app.js
+++ 2/app.js
@@ -5,4 +5,7 @@ function add(n1, n2) {
 function showResult(num) {
     console.log('Result:' + num);
 }
+var combineValues; //Combined values should accept any function that takes two parameter that is number and should return a number
+combineValues = add;
+console.log(combineValues(8, 8));
 showResult(add(5, 12));
--- 2/app.ts
+++ 2/app.ts
@@ -4,8 +4,12 @@ function add(n1:number, n2:number): number {
     return n1 + n2; //here return a number
 }
 
-function showResult(num: number):void {
+function showResult(num: number):void { //White
     console.log('Result:' + num);
 }
 
+let combineValues: (a:number, b:number) => number; //Combined values should accept any function that takes two parameter that is number and should return a number
+
+combineValues = add;
+console.log(combineValues(8, 8));
 showResult(add(5, 12));
