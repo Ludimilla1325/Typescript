34c713a2ca1d404ad26da9aa513b74eadc19d233
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-01-28 23:55:37 -0300

    RRest params

--- 4-next-gen-01-starting-setup/src/app.ts
+++ 4-next-gen-01-starting-setup/src/app.ts
@@ -1,16 +1,16 @@
 // Arrow functions
 
-const add = (a:number, b:number = 1) => a + b;
+// const add = (a:number, b:number = 1) => a + b;
 
-const printOutPut: (a:number | string) => void = output => console.log(output);
+// const printOutPut: (a:number | string) => void = output => console.log(output);
 
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-if (button){
-    button.addEventListener('click', event => console.log(event));
-}
+// if (button){
+//     button.addEventListener('click', event => console.log(event));
+// }
 
-printOutPut(add(5));
+// printOutPut(add(5));
 
 //PUSH - JAVASCRIPT
 
@@ -24,4 +24,15 @@ const person ={
     age:30
 };
 
-const copiedPerson = { ...person };
+const copiedPerson = { ...person }; //Using the {...}, you get a perfect copy of person, you are not only copy but u r doing a pull of the key value pars
+
+//REST Parameters
+
+const add = (...numbers: number[]) =>{ // here accept many arguments as i get
+    return numbers.reduce((curResult, curValue) =>{
+        return curResult + curValue;
+    }, 0);
+};
+
+const addedNumbers = add(10, 20, 30,3);
+console.log(addedNumbers);
