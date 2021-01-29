43156f131ec3be1c58069668cfeab13e599f0b29
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-01-28 23:16:55 -0300

    The spread Operator {...}/ Push

--- 4-next-gen-01-starting-setup/src/app.ts
+++ 4-next-gen-01-starting-setup/src/app.ts
@@ -11,3 +11,17 @@ if (button){
 }
 
 printOutPut(add(5));
+
+//PUSH - JAVASCRIPT
+
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+
+activeHobbies.push(...hobbies);
+
+const person ={
+    name:'Max',
+    age:30
+};
+
+const copiedPerson = { ...person };
