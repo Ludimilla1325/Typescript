cf79497b4c36724634ba5c05b10c1f3fded8d031
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-02-01 22:42:01 -0300

    private and public acess modifiers

--- 5-cl-interfaces/src/app.ts
+++ 5-cl-interfaces/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-    name: string;
+    public name: string; // it can be used out of class
+    private employees: string[]= []; //private makes it private only to what s inside class
 
     constructor(n: string){
         this.name = n; 
@@ -8,11 +9,25 @@ class Department {
     describe(){
         console.log('Department: '+ this.name); // We use this to refer to a class property or a method from inside of the class
     }
+    addEmployee(employee:string){
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+//accounting.employees[2] = "Anna"; //This is not possible cuz is private
+
 accounting.describe();
+accounting.name = 'New name';
+accounting.printEmployeeInformation();
 
 const accountingCopy = {name:'whatever', describe:accounting.describe};
 
