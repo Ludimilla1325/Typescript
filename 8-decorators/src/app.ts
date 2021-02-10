d33f4fabb9ff3a5ce2dd48eccd882e4a453872fd
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-02-10 16:39:34 -0300

    DECORATORS

---
+++ 8-decorators/src/app.ts
@@ -0,0 +1,10 @@
+/* DECORATORS
+
+Meta- programming -  ypu wont use tech writers that often to have a direct impact on the end users of your page but 
+decorators are a intrument to write code which is easier to use by others developers
+With Decorators we can guarantee that one of our classes get users correctly, or a method in a class.
+
+MODULE CONTENT
+*/
+
+
