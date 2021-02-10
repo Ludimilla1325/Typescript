f64c925f6b9945b3cdd3233675530e2037047175
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-02-09 21:44:54 -0300

    Working with Constraints

--- 7-generics/src/app.ts
+++ 7-generics/src/app.ts
@@ -11,11 +11,11 @@
 
 
 
-//GENERIC FUNCTION
+//GENERIC FUNCTION - Generics allow you to continue working with your data in a typescript optimal way.
 
-function merge<T, U>(objA: T, objB: U){  //This function return thhe intersection of T & U
+//Constraints - you set it regarding the types your generic types can be based on, you use  "extends object" and in that u gonna say the "T" type. We guarantee that we get two objects here by setting certain constraints, and it can be anything that we set like objects, string , number , union types, etc
+function merge<T extends object, U extends object>(objA: T, objB: U){  //This function return thhe intersection of T & U
     return Object.assign(objA, objB);
 }
-
 const mergeObj = merge({name: 'Max', hobbies: ['Sports']}, {age:30}); //typescript simply infers the types of the value we are passing as argumet
 console.log(mergeObj);
