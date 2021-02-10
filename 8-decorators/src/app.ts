241a455b163a4f44cd4f2b17ebc82cefb4609255
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-02-10 18:34:23 -0300

    Working with Decorator Factory

--- 8-decorators/src/app.ts
+++ 8-decorators/src/app.ts
@@ -1,12 +1,17 @@
 // You must on "experimentalDecorators": true in tsconfig, to use decorators
 
-function Logger( constructor: Function){
-    console.log('Logging...');
-    console.log(constructor)
+// we can create a DECORATOR FACTORY that return a decorator function but allow  us to configura it when we assign it as a decorator assign
+
+// We can customize the values  the decorator function uses when ti executes with our factory function, so it can give us more power and possibilities of configuring what the decorator does internally
+function Logger(logString: string){ 
+    return function( constructor: Function){
+        console.log(logString);
+        console.log(constructor)
+    }
 }
 
 // After @ should point a function which should be ur decorator
-@Logger
+@Logger('LOGGING - PERSON')
 class Person {
     name = 'Max';
 
