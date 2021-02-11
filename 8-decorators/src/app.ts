78092b7054f29161fc6b30db0bcbdf7c0df3321d
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-02-10 23:02:50 -0300

    Building more useful Decorator 1

--- 8-decorators/src/app.ts
+++ 8-decorators/src/app.ts
@@ -10,8 +10,18 @@ function Logger(logString: string){
     }
 }
 
+// DECORATOR FACTORY
+function withTemplate(template: string, hookId: string){ // Here i want render some template which should be some html code into some place in the DOM,
+    return function (_: Function){ // _ we use that before to say that we are aware of it but that we dont want to use it
+        const hookEl = document.getElementById(hookId);
+        if (hookEl){ //I want render this here
+            hookEl.innerHTML = template;
+        }
+    }
+}
 // After @ should point a function which should be ur decorator
-@Logger('LOGGING - PERSON')
+// @Logger('LOGGING - PERSON')
+@withTemplate('<h1>My person object</h1>', 'app')
 class Person {
     name = 'Max';
 
