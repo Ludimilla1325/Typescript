404415b9fd422c7bbb099985c9c08d3616df2c8b
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-02-19 13:35:31 -0300

    Example: creating an Autobind decorator

--- 8-decorators/src/app.ts
+++ 8-decorators/src/app.ts
@@ -105,18 +105,33 @@ const p2 = new Product('Book 2', 29);
 
 // console.log(p2);
 
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value;
+    const adjDescriptor : PropertyDescriptor = {
+        configurable: true, 
+        enumerable: false,
+
+        get(){
+            const boundFn = originalMethod.bind(this); //this will refer to the object on which we originally defined the method
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
 
 class Printer {
     message = 'This works';
 
-
+    @Autobind // We created a function so we dont need call the bind everywhere 
     showMessage(){
         console.log(this.message);
     }
 }
 
 const p = new Printer();
+p.showMessage();
 
 const button = document.querySelector('button');
-button.addEventListener('click', p.showMessage.bind(p)); // this bind(p), doesnt refer to the EventListener but to the new Printer()
+button?.addEventListener('click', p.showMessage);
 
