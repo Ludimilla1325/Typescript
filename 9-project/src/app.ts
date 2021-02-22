bad1553a0d702ed0facc2f201aeacfb308fc7194
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-02-22 10:57:14 -0300

    Interacting with DOM elements

--- 9-project/src/app.ts
+++ 9-project/src/app.ts
@@ -3,6 +3,10 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+    
     constructor(){
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement; // it wont be null (!) and the type will be HTMLTemplateElement
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
@@ -11,9 +15,29 @@ class ProjectInput {
             this.templateElement.content,
             true
         );
+        // ---- form element
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+       
+        // to get acess in the elements
+       this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+       this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+       this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+       this.configure();
         this.attach(); // we must add the attach so the code will execute,
     }   
+
+    //Listener to our form
+
+    private submitHandler(event: Event){ //receive an event object
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure(){
+        this.element.addEventListener('submit', this.submitHandler.bind(this)); // bind is to pre configure how this function is going to execute, the first argument is what is the "this" keyword will refer to
+    }
 
     private attach(){
         this.hostElement.insertAdjacentElement('afterbegin', this.element) //we gonna insert that after the begining
