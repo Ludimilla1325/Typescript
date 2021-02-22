1ffc5e61d5302fa89f3ed19125177b662de9ea17
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-02-22 10:24:07 -0300

    DOM Element Section & OPP Rendering

--- 9-project/src/app.ts
+++ 9-project/src/app.ts
@@ -1,1 +1,23 @@
-// Code goes here!
+// ---- To get the acess to all the elements (but we r not rendering anything)
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    constructor(){
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement; // it wont be null (!) and the type will be HTMLTemplateElement
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+        
+        const importedNode = document.importNode(
+            this.templateElement.content,
+            true
+        );
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.attach(); // we must add the attach so the code will execute,
+    }   
+
+    private attach(){
+        this.hostElement.insertAdjacentElement('afterbegin', this.element) //we gonna insert that after the begining
+    }
+}
+
+const prjInput = new ProjectInput();
