afee6b7f7d197bec9a832ed878acb582d0c143b2
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-02-23 00:44:36 -0300

    Rendering project Lists

--- 9-project/src/app.ts
+++ 9-project/src/app.ts
@@ -55,6 +55,39 @@ function autobind(
     return adjDescriptor;
 }
 
+// ProjectList Class
+class ProjectList{
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor(private type: 'active' | 'finished'){
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement; // it wont be null (!) and the type will be HTMLTemplateElement
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+        
+        const importedNode = document.importNode(
+            this.templateElement.content,
+            true
+        );
+        // ---- form element
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent(){
+        const listId = `${this.type}-projects-lists`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + 'PROJECTS';
+
+    }
+
+    private attach(){
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
+
 
 //Project Input Class
 // ---- To get the acess to all the elements (but we r not rendering anything)
@@ -149,3 +182,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
